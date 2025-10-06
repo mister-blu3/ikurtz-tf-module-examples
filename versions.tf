e282f662764b6dde8fe9e4c9e1437bbab4f4e02e
Ian Kurtz <ian.kurtz@harness.io>
2025-10-06 17:25:41 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.8.0"
+      version = "~> 2.0.0"
     }
   }
   required_version = ">= 1.0.0"
